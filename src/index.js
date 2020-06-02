3cb8e9531dde95f9fe93e4c8937aa7f8f86e9db9
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-06-02 16:55:23 -0400

    Adds reset functionality

--- src/index.js
+++ src/index.js
@@ -8,31 +8,51 @@ function formatNumberAsTime(value) {
 }
 
 // given an amount of time as total centiseconds, formats it as a timer string
-function formatMillisecondsTime(time) {
-    let centiseconds = time % 100;
-    let seconds = Math.floor(time / 100) % 60;
-    let minutes = Math.floor(time / 6000);
+function updateTimer() {
+    let centiseconds = centisecondsOnTimer % 100;
+    let seconds = Math.floor(centisecondsOnTimer / 100) % 60;
+    let minutes = Math.floor(centisecondsOnTimer / 6000);
 
-    return `${formatNumberAsTime(minutes)}:${formatNumberAsTime(seconds)}.${formatNumberAsTime(centiseconds)}`
+    let timerString = `${formatNumberAsTime(minutes)}:${formatNumberAsTime(seconds)}.${formatNumberAsTime(centiseconds)}`
+
+    document.getElementById("timer").innerHTML = timerString;
 }
 
 function incrementTimer() {
     centisecondsOnTimer += 1;
-    document.getElementById("timer").innerHTML = formatMillisecondsTime(centisecondsOnTimer);
+    updateTimer();
+    
+}
+
+function lapOrResetTimer() {
+    let lapResetButton = document.getElementById("lapResetButton");
+    if (isCounting) {
+        // trigger a lap
+    } else {
+        // trigger a reset
+        centisecondsOnTimer = 0;
+        updateTimer();
+        lapResetButton.disabled = true;
+        lapResetButton.innerHTML = "Lap";
+    }
 }
 
 function toggleTimer() {
     let toggleButton = document.getElementById("toggleButton")
+    let lapResetButton = document.getElementById("lapResetButton");
     if (isCounting) {
         // stop timer, change button back to start timer, change lap button to reset
         clearInterval(countingIntervalId);
         toggleButton.innerHTML = "Start";
+        lapResetButton.innerHTML = "Reset";
     } else {
         // start timer, change button to stop timer, enable lap button
         countingIntervalId = setInterval(incrementTimer, 10);
         toggleButton.innerHTML = "Stop";
+        lapResetButton.disabled = false;
     }
     isCounting = !isCounting;
 }
 
 document.getElementById("toggleButton").addEventListener("click", toggleTimer);
+document.getElementById("lapResetButton").addEventListener("click", lapOrResetTimer);
