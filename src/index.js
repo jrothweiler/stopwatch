12b5cd97d3a061bb55e6de5e0c6e38319d5b9aaa
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-06-02 17:30:06 -0400

    adds lap functionality

--- src/index.js
+++ src/index.js
@@ -2,38 +2,67 @@ var isCounting = false;
 var countingIntervalId = -1;
 var centisecondsOnTimer = 0;
 
+// Contains an array of lap times, or how long it took to complete each lap.
+// This is the time that will be shown in the list for that lap.
+var lapTimes = [];
+
+// this is the total time on the stopwatch when the last time was taken, 
+// for use in calculating the next laps total time. 
+// Initially 0 for calculating the first laps time against the beginning.
+var lastLapStopwatchTime = 0;
+
 // format a number for use in the timer, i.e. pad numbers less than 10 with leading zeroes
 function formatNumberAsTime(value) {
     return value.toString().padStart(2, '0');
 }
 
-// given an amount of time as total centiseconds, formats it as a timer string
+// formats the given centisecond time in MM:SS.CC notation.
+function formatTimeForTimer(time) {
+    let centiseconds = time % 100;
+    let seconds = Math.floor(time / 100) % 60;
+    let minutes = Math.floor(time / 6000);
+
+    return `${formatNumberAsTime(minutes)}:${formatNumberAsTime(seconds)}.${formatNumberAsTime(centiseconds)}`
+}
+
+// updates the timer html with the current centiseconds state,
+// properly formatted.
 function updateTimer() {
-    let centiseconds = centisecondsOnTimer % 100;
-    let seconds = Math.floor(centisecondsOnTimer / 100) % 60;
-    let minutes = Math.floor(centisecondsOnTimer / 6000);
+    document.getElementById("timer").innerHTML = formatTimeForTimer(centisecondsOnTimer);
+}
 
-    let timerString = `${formatNumberAsTime(minutes)}:${formatNumberAsTime(seconds)}.${formatNumberAsTime(centiseconds)}`
-
-    document.getElementById("timer").innerHTML = timerString;
+function updateLapList() {
+    let lapListElement = document.getElementById("lapList");
+    console.log(lapListElement);
+    // for now, just separating times by spaces is fine.
+    lapListElement.innerHTML = lapTimes.join(" ");
 }
 
 function incrementTimer() {
     centisecondsOnTimer += 1;
     updateTimer();
-    
 }
 
 function lapOrResetTimer() {
     let lapResetButton = document.getElementById("lapResetButton");
     if (isCounting) {
         // trigger a lap
+        let lapDuration = centisecondsOnTimer - lastLapStopwatchTime;
+        let lapDurationString = formatTimeForTimer(lapDuration);
+        lapTimes.push(lapDurationString);
+        lastLapStopwatchTime = centisecondsOnTimer;
+        updateLapList();
     } else {
-        // trigger a reset
+        // trigger a reset: set timer to 0, go back to disabled lap button, clear laps
         centisecondsOnTimer = 0;
         updateTimer();
+        
         lapResetButton.disabled = true;
         lapResetButton.innerHTML = "Lap";
+
+        lapTimes = [];
+        lastLapStopwatchTime = 0;
+        updateLapList();
     }
 }
 
