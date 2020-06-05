942a16dcbae8b5e77b6b3ba44cc156b19bf1f347
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-06-05 15:13:46 -0400

    Some refactoring to decouple UI work from logic work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//----------- State ---------------
 let isCounting = false;
 let countingIntervalId = -1;
 let millisecondsPaused = 0;
@@ -22,6 +23,8 @@ let lastStopwatchToggleTime = null;
 // Date the stopwatch was first started at. 
 let startingTime = null;
 
+//----------- UI handlers ---------------
+
 // format a number for use in the timer, i.e. pad numbers less than 10 with leading zeroes
 function padNumber(value) {
     return value.toString().padStart(2, '0');
@@ -39,63 +42,82 @@ function formatTimeForTimer(elapsedTime) {
     return `${minutes}:${seconds}.${centiseconds}`
 }
 
-// updates the timer html with the current centiseconds state,
-// properly formatted.
-function updateTimer() {
+// updates our UI in reaction to a change in time on the timer.
+function onTimerChange(newTime) {
     let timer = document.getElementById("timer");
-    let currentTime = Date.now();
-    let totalTime = currentTime - startingTime - millisecondsPaused;
-    timer.innerText = formatTimeForTimer(totalTime);
+    timer.innerText = formatTimeForTimer(newTime);
+
+    if (newTime == 0) {
+        let lapResetButton = document.getElementById("lapResetButton");
+        lapResetButton.disabled = true;
+        lapResetButton.innerText = "Lap";
+    }
 }
 
-// update lap list html with the new time entry
-function updateLapList(newTime) {
+// updates our UI in reaction to a change in lap list
+function onLapsChange(newLaps) {
     let lapTable = document.getElementById("lapTable");
+    lapTable.textContent = '';
+    for (let i =  1; i <= newLaps.length; i++) {
+        let currentLapTime = newLaps[i - 1];
+        let tableEntry = document.createElement('tr');
 
-    // Create new table entry for new time
-    let tableEntry = document.createElement('tr');
-
-    let lapNumberElement = document.createElement('td');
-    lapNumberElement.innerText = `Lap ${lapTable.children.length + 1}`;
-
-    let lapTimeElement = document.createElement('td');
-    lapTimeElement.innerText = formatTimeForTimer(newTime); 
-
-    // append the columns to the row, then the row to the table
-    tableEntry.appendChild(lapNumberElement);
-    tableEntry.appendChild(lapTimeElement);
-    lapTable.prepend(tableEntry);
-
-    // Iterate through new set of children and update color classes
-    let currentLapRows = lapTable.children;
-    for (let i = 0; i < currentLapRows.length; i++) {
-        let currentRowTime = lapTimes[i];
-        let currentRowElement = currentLapRows[i];
-        let currentRowElementClasses = currentRowElement.classList;
-
-        currentRowElementClasses.remove('bestTime');
-        currentRowElementClasses.remove('worstTime');
-        
-        // Ignore case where there is only one lap, making best and worst equal
         if (bestLapTime != worstLapTime) {
-            if (bestLapTime == currentRowTime) {
-                currentRowElementClasses.add('bestTime');
-            } else if (worstLapTime == currentRowTime) {
-                currentRowElementClasses.add('worstTime');
+            if (bestLapTime == currentLapTime) {
+                tableEntry.classList.add('bestTime');
+            } else if (worstLapTime == currentLapTime) {
+                tableEntry.classList.add('worstTime');
             } 
         }
 
-        if (currentLapRows.length >= 10) {
-            let lapTimeElement = currentRowElement.children[1];
-            lapTimeElement.classList.add('scrollVisible')
-        }
+        // create each column, one with lap number, the other with time
+        let lapNumber = document.createElement('td');
+        lapNumber.innerText = `Lap ${i}`;
+
+        let lapTime = document.createElement('td');
+        lapTime.innerText = formatTimeForTimer(currentLapTime); 
+
+        // append the columns to the row, then the row to the table
+        tableEntry.appendChild(lapNumber);
+        tableEntry.appendChild(lapTime);
+        lapTable.prepend(tableEntry);
     }
+}
+
+// updates our UI in reaction to a change in running state, either 
+// paused to running or running to paused
+function onTimerToggle(isNowRunning) {
+    let toggleButton = document.getElementById("toggleButton");
+    let lapResetButton = document.getElementById("lapResetButton");
+    if (isNowRunning) {
+        toggleButton.innerText = "Stop";
+        toggleButton.classList.add("started");
+        toggleButton.classList.remove("stopped");
+        
+        lapResetButton.innerText = 'Lap';
+    } else {
+        toggleButton.innerText = "Start";
+        toggleButton.classList.add("stopped");
+        toggleButton.classList.remove("started");
+
+        lapResetButton.innerText = 'Reset';
+    }
+    lapResetButton.disabled = false;
+}
+
+//----------- Logic handlers ---------------
+
+// updates the timer html with the current centiseconds state,
+// properly formatted.
+function updateTimer() {
+    let currentTime = Date.now();
+    let totalTime = currentTime - startingTime - millisecondsPaused;
+    onTimerChange(totalTime);
 }
 
 // functionality for left button, which is either reset or lap
 // depending on if the stopwatch is running.
 function lapOrResetTimer() {
-    let lapResetButton = document.getElementById("lapResetButton");
     if (isCounting) {
         let currentTime = Date.now();
         // trigger a lap: calculate new lap time
@@ -113,7 +135,7 @@ function lapOrResetTimer() {
         lapTimes.unshift(lapDuration);
         
         lastLapStopwatchTime = currentTime;
-        updateLapList(lapDuration);
+        onLapsChange(lapTimes);
         millisecondsPausedSinceLastLap = 0;
     } else {
         // trigger a reset: set timer to 0
@@ -121,67 +143,42 @@ function lapOrResetTimer() {
         millisecondsPausedSinceLastLap = 0;
         lastStopwatchToggleTime = null;
         startingTime = null;
-        document.getElementById('timer').innerText = '00:00.00';
 
         bestLapTime = Infinity;
         worstLapTime = -Infinity;
 
-        // go back to disabled lap button
-        lapResetButton.disabled = true;
-        lapResetButton.innerText = "Lap";
-
         // clear laps, clear table
         lapTimes = [];
         lastLapStopwatchTime = null;
-        document.getElementById("lapTable").innerText = '';
+
+        onLapsChange(lapTimes);
+        onTimerChange(0);
     }
 }
 
 // functionality for right button, which starts or stops the timer.
 function toggleTimer() {
     let currentDate = Date.now();
-    let toggleButton = document.getElementById("toggleButton")
-    let lapResetButton = document.getElementById("lapResetButton");
     if (isCounting) {
-        // stop timer
         clearInterval(countingIntervalId);
-
-        // change button back to start timer
-        toggleButton.innerText = "Start";
-        toggleButton.classList.add("stopped");
-        toggleButton.classList.remove("started");
-
-        // change lap button to reset
-        lapResetButton.innerText = "Reset";
     } else {
         // start timer 
         countingIntervalId = setInterval(updateTimer, 10);
-        
-        // change button to stop timer
-        toggleButton.innerText = "Stop";
-        toggleButton.classList.add("started");
-        toggleButton.classList.remove("stopped");
-
-        // enable lap button
-        lapResetButton.innerText = "Lap";
-        lapResetButton.disabled = false;
-
 
         if (startingTime) {
-            // if this is resuming after a pause, calculate pause time and add 
-            // milliseconds paused both overall and since last lap
+            // case for resuming after a pause, 
             let pauseTime = currentDate - lastStopwatchToggleTime;
             millisecondsPaused += pauseTime;
             millisecondsPausedSinceLastLap += pauseTime;
         } else {
-            // else, if we are starting for the first time,
-            // note the starting time.
+            // else, we are starting for the first time,
             startingTime = currentDate;
             lastLapStopwatchTime = currentDate;
         }
     }
     lastStopwatchToggleTime = currentDate;
     isCounting = !isCounting;
+    onTimerToggle(isCounting);
 }
 
 document.getElementById("toggleButton").addEventListener("click", toggleTimer);
